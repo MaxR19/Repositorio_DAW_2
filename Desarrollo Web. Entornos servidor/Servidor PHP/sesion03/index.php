5d8d0b4b24158400a02b00a1fa0435a7496249dc
MaxR19 <gomezsuarez.tonnyf2020@gmail.com>
2025-11-06 14:56:17 +0100

    Commit 06.11.2025 (4)

--- Desarrollo Web. Entornos servidor/Servidor PHP/sesion03/index.php
+++ Desarrollo Web. Entornos servidor/Servidor PHP/sesion03/index.php
@@ -1,5 +1,15 @@
-<?php $idioma = $_POST['idioma'];
-if ($idioma == "es") {
-        $bienvenida = "Tu nombre completo es ";
-         
-}
+<?php
+
+foreach
+
+?>
+
+<form action="index.php" method="POST">
+        <label>Editorial: </label><br/>
+        <select name="interes[]" size="4">
+                <option value="Marvel">Marvel</option>
+                <option value="DC">DC</option>
+                <option value="Image">Image</option>
+        </select>
+        <label>Guionistas:</label><br/>
+        
